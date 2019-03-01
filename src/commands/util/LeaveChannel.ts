b4f9f3bab98a212774239ebea51cb65d897c978f
John <jpalbert81@gmail.com>
2019-02-28 19:09:34 -0800

    add leave command

---
+++ src/commands/util/LeaveChannel.ts
@@ -0,0 +1,27 @@
+'use strict';
+import { Command, CommandoClient, CommandMessage } from 'discord.js-commando';
+import { Message } from 'discord.js';
+
+
+module.exports = class LeaveChannel extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'leave',
+      group: 'util',
+      memberName: 'leave',
+      description: 'Tells the bot to leave any voice channel it\'s in'
+    });
+  }
+
+  async run(msg: CommandMessage) {
+
+    return new Promise<Message>(() => {
+      if (msg.guild.voiceConnection) {
+        msg.reply('Ok bye... :weary:');
+        msg.guild.voiceConnection.disconnect();
+      } else {
+        msg.reply('I\'m not in a voice channel :sob:');
+      }
+    });
+  }
+}
