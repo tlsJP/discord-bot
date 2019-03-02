b49aa39c53caf0bf67facf39922dcbd4674fb291
John <jpalbert81@gmail.com>
2019-03-02 10:33:46 -0800

    format listing

--- src/commands/music/List.ts
+++ src/commands/music/List.ts
@@ -4,6 +4,8 @@ import { Message } from 'discord.js';
 import * as fs from 'fs';
 import * as repo from './WorkingDirectory.json';
 
+const DESIRED_COLUMN_WIDTH: number = 6;
+
 module.exports = class Play extends Command {
   constructor(client: CommandoClient) {
     super(client, {
@@ -16,7 +18,6 @@ module.exports = class Play extends Command {
 
   async run(msg: CommandMessage) {
     return new Promise<Message>(() => {
-      console.log('list files');
       fs.readdir(repo.workingDirectory, (err, files) => {
 
         if (err) {
@@ -24,18 +25,44 @@ module.exports = class Play extends Command {
           return;
         }
 
-        let str = "\nMP3s\n\n";
-        str += "Usage : !play {sound}\n";
-        str += "\nSounds:\n";
-        let i = 0;
-        files.forEach(f => {
-          if (f.endsWith('.mp3') && i < 50) {
-            str += '  ' + f.replace('.mp3', '') + '\n';
-            i++;
-          }
+        console.log("Number of files found : " + files.length);
+        let maxFileName = 1;
+        files.forEach((f) => {
+          maxFileName = f.length - 3 > maxFileName ? f.length - 3 : maxFileName;
         })
 
-        msg.reply(str)
+        let str = "\nMP3s\n\n";
+        str += "Usage : !play {sound}\n\n";
+        str += '`';
+        str += "\nSounds:\n";
+
+
+        for (let i = 0; i < files.length;) {
+          let f = files[i];
+
+          // Create row of sounds
+          let row = [];
+          for (let j = 0; j < DESIRED_COLUMN_WIDTH;) {
+            if (!f) {
+              break;
+            }
+            if (f.endsWith('.mp3')) {
+              row[j] = f.replace('.mp3', '').padStart(maxFileName, ' ');
+              j++;
+            }
+
+            // Don't forget to move the file array pointer
+            i++;
+            f = files[i];
+
+          }
+
+          str += row.join("") + "\n";
+
+        }
+
+
+        msg.reply(str + '`')
           .catch(err => {
             console.log(err);
             console.log(str.length);
