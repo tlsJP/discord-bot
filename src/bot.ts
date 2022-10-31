28dfbf7dd298f4d0b19d9d71963f15d4d343dd79
John Albert <jpalbert81@gmail.com>
2022-10-30 20:15:16 -0700

    fix login

--- src/bot.ts
+++ src/bot.ts
@@ -1,39 +1,40 @@
-'use strict';
+"use strict";
 
-import { GuildMember } from 'discord.js';
-import * as cmd from './commands.js';
-import * as auth from './auth.json';
-import * as commando from 'discord.js-commando';
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import * as cmd from "./commands.js";
+import * as auth from "./auth.json";
 
-var bot: commando.CommandoClient;
+const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 init();
 
 function init() {
   let d = new Date();
-  console.log(d + ' : Starting the bot!');
-  bot = new commando.CommandoClient();
+  console.log(d + " : Starting the bot!");
 
-  bot.registry
-    .registerGroup('util', 'Utils')
-    .registerGroup('music', 'Music')
-    .registerDefaults()
-    .registerCommandsIn(__dirname + '/commands');
+  bot.once(Events.ClientReady, (c) => {
+    console.log(`Logged in as ${c.user.tag}`);
+  });
 
   bot.login(auth.token);
-
 }
 
 function destroy() {
   let d = new Date();
-  console.log(d + ' : Destroying the bot...');
+  console.log(d + " : Destroying the bot...");
   bot.destroy();
 }
 
 function channelChanged(oldMember: GuildMember, newMember: GuildMember) {
-  return (oldMember && oldMember.voiceChannel && oldMember.voiceChannel.name)
-    && (newMember && newMember.voiceChannel && newMember.voiceChannel.name)
-    && oldMember.voiceChannel.name != newMember.voiceChannel.name;
+  return (
+    oldMember &&
+    oldMember.voiceChannel &&
+    oldMember.voiceChannel.name &&
+    newMember &&
+    newMember.voiceChannel &&
+    newMember.voiceChannel.name &&
+    oldMember.voiceChannel.name != newMember.voiceChannel.name
+  );
 }
 
 function firstChannel(oldMember: GuildMember, newMember: GuildMember) {
@@ -43,50 +44,48 @@ function firstChannel(oldMember: GuildMember, newMember: GuildMember) {
 /**
  * 'Welcome' people who join a voice channel
  */
-bot.on('voiceStateUpdate', (oldMember: GuildMember, newMember: GuildMember) => {
-
+bot.on("voiceStateUpdate", (oldMember: GuildMember, newMember: GuildMember) => {
   //only play welcome if the voice channel changed or if they were not previously in a voice channel
-  if (channelChanged(oldMember, newMember) || firstChannel(oldMember, newMember)) {
-    cmd.playFile(newMember, 'sup');
+  if (
+    channelChanged(oldMember, newMember) ||
+    firstChannel(oldMember, newMember)
+  ) {
+    cmd.playFile(newMember, "sup");
   }
-
 });
 
-bot.on('ready', () => {
-  console.log('Logged in as %s', bot.user.username);
+bot.on("ready", () => {
+  console.log("Logged in as %s", bot.user.username);
 });
 
-bot.on('error', (e: Error) => {
+bot.on("error", (e: Error) => {
   let d = new Date();
-  console.log(d.toString() + ' : Oh no an error!');
+  console.log(d.toString() + " : Oh no an error!");
   console.log(e);
   destroy();
   process.exit(1);
-})
+});
 
-bot.on('message', msg => {
-
+bot.on("message", (msg) => {
   let content = msg.content;
 
-  if (content.substring(0, 1) == '!') {
-    let args = content.substring(1).split(' ');
+  if (content.substring(0, 1) == "!") {
+    let args = content.substring(1).split(" ");
     let req = args[0];
     args.splice(0, 1);
 
     let d = new Date();
-    console.log(d.toString() + ' [' + msg.member.user.username + ']: ' + content);
+    console.log(
+      d.toString() + " [" + msg.member.user.username + "]: " + content
+    );
 
     switch (req) {
-
-      case 'echo':
-        msg.reply(args.join(' '));
+      case "echo":
+        msg.reply(args.join(" "));
         break;
-
 
       default:
       // msg.reply(':angry: Unrecognized command!');
-
     }
   }
-
 });
