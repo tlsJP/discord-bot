df3819c062005280e671a9bc8c1127983093c6eb
John Albert <jpalbert81@gmail.com>
2022-10-30 22:33:50 -0700

    workin on it

--- src/commands/util/JoinChannel.ts
+++ src/commands/util/JoinChannel.ts
@@ -1,30 +1,47 @@
-'use strict';
-import { Command, CommandoClient, CommandMessage } from 'discord.js-commando';
-import { Message } from 'discord.js';
+"use strict";
 
-module.exports = class JoinChannel extends Command {
-  constructor(client: CommandoClient) {
-    super(client, {
-      name: 'join',
-      group: 'util',
-      memberName: 'join',
-      description: 'Joins the voice channel of the commander'
-    });
-  }
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  GuildMember,
+} from "discord.js";
 
-  async run(msg: CommandMessage) {
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("join")
+    .setDescription("Joins the voice channel of the commander"),
+  async execute(interaction: CommandInteraction) {
+    let channelId = (interaction.member as GuildMember).voice.channelId;
+    if (channelId) {
+    } else {
+      await interaction.reply("I don't  know where to go :cry:");
+    }
+  },
+};
 
-    return new Promise<Message>(() => {
-      if (msg.member.voiceChannel) {
-        msg.member.voiceChannel.join()
-          .then(() => {
-            msg.reply('Here I am!! :hugging:');
-          })
-          .catch(console.log);
+// module.exports = class JoinChannel extends Command {
+//   constructor(client: CommandoClient) {
+//     super(client, {
+//       name: 'join',
+//       group: 'util',
+//       memberName: 'join',
+//       description: 'Joins the voice channel of the commander'
+//     });
+//   }
 
-      } else {
-        msg.reply('I don\'t know where to go :cry:');
-      }
-    });
-  }
-}
+//   async run(msg: CommandMessage) {
+
+//     return new Promise<Message>(() => {
+//       if (msg.member.voiceChannel) {
+//         msg.member.voiceChannel.join()
+//           .then(() => {
+//             msg.reply('Here I am!! :hugging:');
+//           })
+//           .catch(console.log);
+
+//       } else {
+//         msg.reply('I don\'t know where to go :cry:');
+//       }
+//     });
+//   }
+// }
